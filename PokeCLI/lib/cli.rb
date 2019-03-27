a35ab76fce120dacbc986226ec810f8888836c7d
“Ranjit <ranjit.saimbi@gmail.com>
2019-03-27 14:14:56 +0000

    CLI Changes

--- PokeCLI/lib/cli.rb
+++ PokeCLI/lib/cli.rb
@@ -1,7 +1,7 @@
 # include Styling
 require_relative './styling'
 
-class CLI 
+class CLI
   include Styling
   # require 'pry'
   attr_reader :user
@@ -9,20 +9,20 @@ class CLI
 
   def loading_screen
     puts_super_fast <<-EOF
-    .::.                          
-    .;:**'            AMC           
-    `                  0            
-.:XHHHHk.              db.   .;;.     dH  MX   0            
-oMMMMMMMMMMM       ~MM  dMMP :MMMMMR   MMM  MR      ~MRMN     
+    .::.
+    .;:**'            AMC
+    `                  0
+.:XHHHHk.              db.   .;;.     dH  MX   0
+oMMMMMMMMMMM       ~MM  dMMP :MMMMMR   MMM  MR      ~MRMN
 QMMMMMb  "MMX       MMMMMMP !MX' :M~   MMM MMM  .oo. XMMM 'MMM
 `MMMM.  )M> :X!Hk. MMMM   XMM.o"  .  MMMMMMM X?XMMM MMM>!MMP
-'MMMb.dM! XM M'?M MMMMMX.`MMMMMMMM~ MM MMM XM `" MX MMXXMM 
-~MMMMM~ XMM. .XM XM`"MMMb.~*?**~ .MMX M t MMbooMM XMMMMMP 
-?MMM>  YMMMMMM! MM   `?MMRb.    `"""   !L"MMMMM XM IMMM  
-MMMX   "MMMM"  MM       ~%:           !Mh.""" dMI IMMP  
-'MMM.                                             IMX   
-~M!M                                             IMP   
-                                    
+'MMMb.dM! XM M'?M MMMMMX.`MMMMMMMM~ MM MMM XM `" MX MMXXMM
+~MMMMM~ XMM. .XM XM`"MMMb.~*?**~ .MMX M t MMbooMM XMMMMMP
+?MMM>  YMMMMMM! MM   `?MMRb.    `"""   !L"MMMMM XM IMMM
+MMMX   "MMMM"  MM       ~%:           !Mh.""" dMI IMMP
+'MMM.                                             IMX
+~M!M                                             IMP
+
         A SPECTACULAR GAME BY RANJIT & AZAM
         ___________________________________
     EOF
@@ -38,6 +38,7 @@ MMMX   "MMMM"  MM       ~%:           !Mh.""" dMI IMMP
     puts_fast "           Welcome to PokeCLI - Enter your name below!"
     puts ""
     puts_super_fast PASTEL.yellow("***************************************************************")
+    puts ""
     name = gets.chomp
     @user = User.find_or_create_by(name: name)
     @user.pokemons.count == 0 ? self.default_pokemon : self.menu
@@ -50,7 +51,6 @@ MMMX   "MMMM"  MM       ~%:           !Mh.""" dMI IMMP
     puts ""
     options = ["Bulbasaur", "Squirtle", "Charmander", "Pikachu"]
     choice = PROMPT.select("I see you don't have any Pokemon with you. Well, I've had a few new ones come in just today - would you like to see?", options)
-    choice = gets.chomp.to_i
     puts ""
     case choice
     when options[0]
@@ -62,31 +62,39 @@ MMMX   "MMMM"  MM       ~%:           !Mh.""" dMI IMMP
     when options[3]
       UserPokemon.create({user: @user, pokemon: Pokemon.find_by(name: "Pikachu")})
     end
-    puts ""
     self.menu
   end
 
   def menu
-    puts "Welcome to the Main Menu."
-    puts "Press 1 to do battle!"
-    puts "Press 2 to view your Pokedex"
-    choice = gets.chomp.to_i
+    puts ""
+    options = ["Do Battle", "View Pokedex", "Leaderboard", "Switch User", "About","Exit"]
+    choice = PROMPT.select("Welcome to the Main Menu. Please select an option.", options)
+    puts ""
     case choice
-    when 1
+    when options[0]
       self.battle
-    when 2
+    when options[1]
       self.pokedex
+    when options[2]
+      self.leaderboard
+    when options[3]
+      self.switch_user
+    when options[4]
+      self.about
+    when options[5]
+      self.exit
     end
   end
 
 
     def select_pokemon
-      puts "Woah - I think I saw Team Rocket up ahead! Quick - select your pokemon for battle!"
-      @user.pokemons.all.each {|pokemon| puts "#{pokemon.name}"}
-      puts "Type the name of the pokemon you want to select."
-      choice = gets.chomp
+      puts_fast "Woah - I think I saw Team Rocket up ahead! Quick - select your pokemon for battle!"
+      puts ""
+      options = @user.pokemons.all.map {|pokemon| pokemon.name}
+      choice = PROMPT.select("Type the name of the pokemon you want to select.", options)
       @current_selected_pokemon = Pokemon.find_by(name: choice)
-      puts "Great! You chose #{@current_selected_pokemon.name}! Let's get ready!"
+      puts_fast "Great! You chose #{@current_selected_pokemon.name}! Let's get ready!"
+      puts ""
     end
 
     def random_pokemon
@@ -102,7 +110,71 @@ MMMX   "MMMM"  MM       ~%:           !Mh.""" dMI IMMP
     end
 
     def pokedex
+      rows = @user.pokemons.map{|p|[p.name, p.speed, p.special_defense, p.special_attack, p.defense, p.attack]}
+      table = Terminal::Table.new :title => "POKEDEX", :headings => ['NAME','SPEED', 'SPECIAL DEFENSE', 'SPECIAL ATTACK', 'DEFENCE', 'ATTACK'], :rows => rows, :style => {:all_separators => true}
+      table.style = {:width => 110, :padding_left => 2, :border_x => "=", :border_i => "+"}
+      puts PASTEL.bright_yellow(table)
+      puts ""
+      options = ["Back to Menu", "Set a Pokemon Free", "Pokemon Left to Catch"]
+      choice = PROMPT.select("What would you like to do?", options)
+      if choice == "Back to Menu"
+        self.menu
+      elsif choice == "Set a Pokemon Free"
+        self.free_pokemon
+      elsif choice == "Pokemon Left to Catch"
+        self.pokemon_left_to_catch
+      end
+    end
 
+    def free_pokemon
+      puts ""
+      options = @user.pokemons.all.map {|pokemon| pokemon.name}
+      choice = PROMPT.select("Please select a pokemon that you would like to set free.", options)
+      pokemon_to_set_free = Pokemon.find_by(name: choice)
+
+      if @user.pokemons.count > 1
+        UserPokemon.all.find_by({user: @user, pokemon: pokemon_to_set_free}).destroy
+        puts ""
+        puts_fast "You set #{pokemon_to_set_free.name} free! Goodbye #{pokemon_to_set_free.name}!"
+        puts ""
+      else
+      puts ""
+      puts_fast "You've only got one pokemon left - you cannot have less than one pokemon."
+      puts ""
+      end
+
+      options = ["Back to menu"]
+      choice = PROMPT.select("What would you like to do?", options)
+      self.menu if choice == "Back to menu"
+    end
+
+    def pokemon_left_to_catch
+      pokemon_left_array = Pokemon.all - @user.pokemons.all
+      i = 0
+      rows = pokemon_left_array.map{|p|["#{i += 1}.", p.name, "?"]}
+      table = Terminal::Table.new :title => "POKEMON LEFT TO CATCH", :headings => ['NO.','NAME.', 'STATS'], :rows => rows, :style => {:all_separators => true}
+      table.style = {:width => 70, :padding_left => 2, :border_x => "=", :border_i => "+"}
+      puts PASTEL.bright_yellow(table)
+      options = ["Back to Menu"]
+      choice = PROMPT.select("What would you like to do?", options)
+      self.menu if choice == "Back to Menu"
+    end
+
+    def leaderboard
+      rows = UserPokemon.group(:user).count.map {|item| [item[0].name, item[1]]}
+      table = Terminal::Table.new :title => "LEADERBOARD", :headings => ['USER', 'POKEMON COUNT'], :rows => rows, :style => {:all_separators => true}
+      table.style = {:width => 70, :padding_left => 2, :border_x => "=", :border_i => "+"}
+      puts PASTEL.bright_yellow(table)
+      options = ["Back to Menu"]
+      choice = PROMPT.select("What would you like to do?", options)
+      self.menu if choice == "Back to Menu"
+    end
+
+    def switch_user
+      
+      options = ["Back to Menu"]
+      choice = PROMPT.select("What would you like to do?", options)
+      self.menu if choice == "Back to Menu"
     end
 
 end
