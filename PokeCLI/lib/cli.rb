e90b1c9a516b20856e87ec1f3ceb9c698e1d51d0
“Ranjit <ranjit.saimbi@gmail.com>
2019-03-28 13:55:29 +0000

    CLI

--- PokeCLI/lib/cli.rb
+++ PokeCLI/lib/cli.rb
@@ -17,15 +17,15 @@ puts ""
 
 
     puts_super_fast <<-EOF
-              ██████╗  ██████╗ ██╗  ██╗███████╗     ██████╗██╗     ██╗
-              ██╔══██╗██╔═══██╗██║ ██╔╝██╔════╝    ██╔════╝██║     ██║
-              ██████╔╝██║   ██║█████╔╝ █████╗█████╗██║     ██║     ██║
-              ██╔═══╝ ██║   ██║██╔═██╗ ██╔══╝╚════╝██║     ██║     ██║
-              ██║     ╚██████╔╝██║  ██╗███████╗    ╚██████╗███████╗██║
-              ╚═╝      ╚═════╝ ╚═╝  ╚═╝╚══════╝     ╚═════╝╚══════╝╚═╝
+                    ██████╗  ██████╗ ██╗  ██╗███████╗     ██████╗██╗     ██╗
+                    ██╔══██╗██╔═══██╗██║ ██╔╝██╔════╝    ██╔════╝██║     ██║
+                    ██████╔╝██║   ██║█████╔╝ █████╗█████╗██║     ██║     ██║
+                    ██╔═══╝ ██║   ██║██╔═██╗ ██╔══╝╚════╝██║     ██║     ██║
+                    ██║     ╚██████╔╝██║  ██╗███████╗    ╚██████╗███████╗██║
+                    ╚═╝      ╚═════╝ ╚═╝  ╚═╝╚══════╝     ╚═════╝╚══════╝╚═╝
 
 
-                          A RANJIT & AZAM PRODUCTION
+                                A RANJIT & AZAM PRODUCTION
           EOF
   end
 
@@ -52,7 +52,7 @@ puts ""
     puts_fast "Hello! My name is Oak. People call me the Pokemon Professor. So your name is #{@user.name}?"
     puts ""
     options = ["Bulbasaur", "Squirtle", "Charmander", "Pikachu"]
-    choice = PROMPT.select("I see you don't have any Pokemon with you. Well, I've had a few new ones come in just today - would you like to see?", options)
+    choice = PROMPT.select("I see you don't have any Pokemon with you. Well, I've had a few new arriavls come in just this morning - would you like to see?", options)
     case choice
     when options[0]
       UserPokemon.create({user: @user, pokemon: Pokemon.find_by(name: "Bulbasaur")})
@@ -68,7 +68,7 @@ puts ""
 
   def menu
     puts ""
-    options = ["Do Battle", "View Pokedex", "Leaderboard", "Instructions", "Exit"]
+    options = ["Battle!", "View Pokedex", "Leaderboard", "Instructions", "Exit"]
     choice = PROMPT.select("Welcome to the Main Menu. Please select an option.", options)
     puts ""
     case choice
@@ -90,7 +90,7 @@ puts ""
       puts_fast "Woah - I think I saw Team Rocket up ahead!"
       puts ""
       options = @user.pokemons.all.map {|pokemon| pokemon.name}
-      choice = PROMPT.select("Quick - select your pokemon for battle!", options)
+      choice = PROMPT.select("Quick - select your Pokemon for battle!", options)
       @current_selected_pokemon = Pokemon.find_by(name: choice)
       puts ""
       puts_fast "Great! You chose #{@current_selected_pokemon.name}! Let's get ready!"
@@ -100,7 +100,8 @@ puts ""
     def random_pokemon
       u = @current_selected_pokemon
       a1 = [u.speed, u.special_defense, u.special_attack, u.defense, u.attack].sort
-      r = Pokemon.order(Arel.sql('random()')).first
+      r = Pokemon.order(Arel.sql('random()')) - @user.pokemons
+      r = r.first
       a2 = [r.speed, r.special_defense, r.special_attack, r.defense, r.attack]
       while a2.none? {|i|i < a1[1]}
         r = Pokemon.order(Arel.sql('random()')).first
@@ -119,6 +120,7 @@ puts ""
     end
 
     def pokedex
+      @user.reload
       rows = @user.pokemons.map{|p|[p.name, p.speed, p.special_defense, p.special_attack, p.defense, p.attack]}
       table = Terminal::Table.new :title => "POKEDEX", :headings => ['NAME','SPEED', 'SPECIAL DEFENSE', 'SPECIAL ATTACK', 'DEFENCE', 'ATTACK'], :rows => rows, :style => {:all_separators => true}
       table.style = {:width => 110, :padding_left => 2, :border_x => "=", :border_i => "+"}
@@ -138,18 +140,24 @@ puts ""
     def free_pokemon
       puts ""
       options = @user.pokemons.all.map {|pokemon| pokemon.name}
-      choice = PROMPT.select("Please select a pokemon that you would like to set free.", options)
+      choice = PROMPT.select("Please select a Pokemon that you would like to set free.", options)
       pokemon_to_set_free = Pokemon.find_by(name: choice)
 
 
       if @user.pokemons.count > 1
-        UserPokemon.all.find_by({user: @user, pokemon: pokemon_to_set_free}).destroy
-        puts ""
-        puts_fast "You set #{pokemon_to_set_free.name} free! Goodbye #{pokemon_to_set_free.name}!"
-        puts ""
+      puts ""
+      choice = PROMPT.yes?("Are you sure you want to set #{pokemon_to_set_free.name} free?")
+        if choice
+          UserPokemon.all.find_by({user: @user, pokemon: pokemon_to_set_free}).destroy
+          puts ""
+          puts_fast "You set #{pokemon_to_set_free.name} free! Goodbye #{pokemon_to_set_free.name}!"
+          puts ""
+        else
+          self.menu
+        end
       else
       puts ""
-      puts_fast "You've only got one pokemon left - you cannot have less than one pokemon."
+      puts_fast "You've only got one Pokemon left - you cannot have less than one pokemon."
       puts ""
       end
 
@@ -165,13 +173,16 @@ puts ""
       table = Terminal::Table.new :title => "POKEMON LEFT TO CATCH", :headings => ['NO.','NAME.', 'STATS'], :rows => rows, :style => {:all_separators => true}
       table.style = {:width => 70, :padding_left => 2, :border_x => "=", :border_i => "+"}
       puts PASTEL.bright_green(table)
+      puts ""
+      puts_fast PASTEL.yellow("#{@user.name} - you still need to catch these Pokemon!")
+      puts ""
       options = ["Back to Menu"]
       choice = PROMPT.select("What would you like to do?", options)
       self.menu if choice == "Back to Menu"
     end
 
     def leaderboard
-      rows = UserPokemon.group(:user).count.map {|item| [item[0].name, item[1]]}
+      rows = UserPokemon.group(:user).count.sort.to_h.map {|item| [item[0].name, item[1]]}
       table = Terminal::Table.new :title => "LEADERBOARD", :headings => ['USER', 'POKEMON COUNT'], :rows => rows, :style => {:all_separators => true}
       table.style = {:width => 70, :padding_left => 2, :border_x => "=", :border_i => "+"}
       puts PASTEL.bright_green(table)
@@ -181,12 +192,22 @@ puts ""
     end
 
     def instructions
-      puts_super_fast "PokeCLI is a turn-based top-trumps game. Each pokemon has five attributes: SPEED, SPECIAL DEFENSE, SPECIAL ATTACK, DEFENCE and ATTACK."
-      puts_super_fast "Battle Team Rocket to try and collect as many pokemon as possible. Select your best pokemon to face Team Rocket!"
-      puts_super_fast "Each battle is made up of a maximum of three rounds. Select your pokemon's best attribute to compare it with your opponent's."
-      puts_super_fast "If you win at least two rounds, you win the battle and your opponent's pokemon which gets added to your pokedex."
-      puts_super_fast "If you lose, you lose your pokemon, so be careful!"
-      puts_super_fast "Good luck out there #{@user.name}!"
+      system("clear")
+      puts_fast PASTEL.cyan("PokeCLI is a turn-based top-trumps game.")
+      puts ""
+      puts_fast PASTEL.cyan("Each Pokemon has five attributes: SPEED, SPECIAL DEFENSE, SPECIAL ATTACK, DEFENCE and ATTACK.")
+      puts ""
+      puts_fast PASTEL.cyan("Battle Team Rocket to try and collect as many Pokemon as possible. Select your best Pokemon to face Team Rocket!")
+      puts ""
+      puts_fast PASTEL.cyan("Each battle is made up of a maximum of three rounds. Select your Pokemon's best attribute to compare it with your opponent's.")
+      puts ""
+      puts_fast PASTEL.cyan("If you win at least two rounds, you win the battle and your opponent's Pokemon.")
+      puts ""
+      puts_fast PASTEL.cyan("If you lose, you lose your Pokemon, so be careful!")
+      puts ""
+      puts_fast PASTEL.cyan("You can inspect all of your Pokemon in the Pokedex.")
+      puts ""
+      puts_fast PASTEL.cyan("Good luck catching Pokemon, #{@user.name}!")
 
       options = ["Back to Menu"]
       choice = PROMPT.select("What would you like to do?", options)
@@ -203,12 +224,12 @@ puts ""
 
         puts_super_fast <<-EOF
 
-         ██████╗  ██████╗  ██████╗ ██████╗ ██████╗ ██╗   ██╗███████╗██╗
-        ██╔════╝ ██╔═══██╗██╔═══██╗██╔══██╗██╔══██╗╚██╗ ██╔╝██╔════╝██║
-        ██║  ███╗██║   ██║██║   ██║██║  ██║██████╔╝ ╚████╔╝ █████╗  ██║
-        ██║   ██║██║   ██║██║   ██║██║  ██║██╔══██╗  ╚██╔╝  ██╔══╝  ╚═╝
-        ╚██████╔╝╚██████╔╝╚██████╔╝██████╔╝██████╔╝   ██║   ███████╗██╗
-         ╚═════╝  ╚═════╝  ╚═════╝ ╚═════╝ ╚═════╝    ╚═╝   ╚══════╝╚═╝
+                 ██████╗  ██████╗  ██████╗ ██████╗ ██████╗ ██╗   ██╗███████╗██╗
+                ██╔════╝ ██╔═══██╗██╔═══██╗██╔══██╗██╔══██╗╚██╗ ██╔╝██╔════╝██║
+                ██║  ███╗██║   ██║██║   ██║██║  ██║██████╔╝ ╚████╔╝ █████╗  ██║
+                ██║   ██║██║   ██║██║   ██║██║  ██║██╔══██╗  ╚██╔╝  ██╔══╝  ╚═╝
+                ╚██████╔╝╚██████╔╝╚██████╔╝██████╔╝██████╔╝   ██║   ███████╗██╗
+                 ╚═════╝  ╚═════╝  ╚═════╝ ╚═════╝ ╚═════╝    ╚═╝   ╚══════╝╚═╝
         EOF
 
 
